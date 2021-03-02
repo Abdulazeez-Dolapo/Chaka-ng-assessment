42ccf7865e811e44571c2a3b65283f14e4df9f8f
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-03-02 15:27:43 +0100

    add sort method

--- src/com/chaka/stockTable/StockTable.java
+++ src/com/chaka/stockTable/StockTable.java
@@ -1,8 +1,10 @@
 package com.chaka.stockTable;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.UUID;
+//import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.lang.*;
+//import java.util.UUID;
 
 public class StockTable {
     public HashMap<Integer, TableData> stockTable;
@@ -22,7 +24,7 @@ public class StockTable {
         this.stockTable.put(index, new TableData(merchantId, userId));
     }
 
-    private HashMap<Integer, TableData> viewStockTable() {
+    public HashMap<Integer, TableData> viewStockTable() {
         return this.stockTable;
     }
 
@@ -30,13 +32,45 @@ public class StockTable {
         this.stockTable = new HashMap<>();
     }
 
+    private static HashMap<Integer, TableData> sortByDate(HashMap<Integer, TableData> dataToSort, String orderBy) {
+        // Create a list from elements of HashMap
+        List<Map.Entry<Integer, TableData> > list =
+                new LinkedList<Map.Entry<Integer, TableData> >(dataToSort.entrySet());
+
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<Integer, TableData> >() {
+            public int compare(Map.Entry<Integer, TableData> first, Map.Entry<Integer, TableData> second) {
+                if (orderBy.equals("asc")) {
+                    return (first.getValue().getCreatedAt()).compareTo(second.getValue().getCreatedAt());
+                } else {
+                    return (second.getValue().getCreatedAt()).compareTo(first.getValue().getCreatedAt());
+                }
+            }
+        });
+
+        // put data from sorted list to hashmap
+        HashMap<Integer, TableData> temp = new LinkedHashMap<Integer, TableData>();
+        for (Map.Entry<Integer, TableData> item : list) {
+            temp.put(item.getKey(), item.getValue());
+        }
+
+        return temp;
+    }
+
+    public HashMap<Integer, TableData> filterTable(String filterKey, String filterValue) {
+        // Validate filterKey and filterValue values (check if empty)
+        // Validate filterValue to be either "asc" or "desc"
+
+        HashMap<Integer, TableData> sorted = sortByDate(this.stockTable, filterValue);
+        return sorted;
+    }
+
     public static void main(String[] args) {
         StockTable stockTable = new StockTable();
         stockTable.stored("merchantId", "userId");
         stockTable.stored("merchantId2", "userId2");
         stockTable.stored("merchantId3", "userId3");
-        System.out.println(stockTable.viewStockTable());
-        System.out.println(stockTable.generateUID());
+        System.out.println(stockTable.filterTable("fg", "desc"));
     }
 }
 
